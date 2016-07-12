bc50587816f9fa5ac01de69657a5359307542c28
solomongamid <sulimanabdulgadeir@gmail.com>
2016-07-12 17:16:04 +0200

    solomongamid

--- pangram.php
+++ pangram.php
@@ -7,7 +7,20 @@
 
 function isPangram($string)
 {
-    //
-    // YOUR CODE GOES HERE
-    //
+   foreach (str_split($text) as $c) {
+        if ($c >= 'a' && $c <= 'z')
+            $bitset |= (1 << (ord($c) - ord('a')));
+        else if ($c >= 'A' && $c <= 'Z')
+            $bitset |= (1 << (ord($c) - ord('A')));
+    }
+    return $bitset == 0x3ffffff;
 }
+ 
+$test = array(
+    "the quick brown fox jumps over the lazy dog",
+    
+);
+ 
+foreach ($test as $str)
+    echo "$str : ", isPangram($str) ? 'T' : 'F', '</br>';
+	?>
